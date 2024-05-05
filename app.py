5a6fbdc70ccf4709695290c69c16376555891a7f
chenBing <chenbingweb@sina.com>
2024-05-05 11:02:22 +0800

    2

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from src.myTrain import Tranner
 mid = 'unsloth/mistral-7b-instruct-v0.2-bnb-4bit'
 dataSrc='/root/autodl-tmp/DISC-Law-SFT-Pair.jsonl'
+saveModel='/root/autodl-tmp/mistral-7b-instruct-v0.2-bnb-4bit-law-v1.0'
+
 import subprocess
 import os
 result = subprocess.run('bash -c "source /etc/network_turbo && env | grep proxy"', shell=True, capture_output=True, text=True)
@@ -9,5 +11,28 @@ for line in output.splitlines():
     if '=' in line:
         var, value = line.split('=', 1)
         os.environ[var] = value
-my_trainer = Tranner(mid,dataSrc)
+
+my_trainer = Tranner(mid,TUNE_MODEL_NAME='mistral-7b-instruct-v0.2-bnb-4bit-law',TUNE_MODEL_VERSION='v1.0')
 my_trainer.loadModel()
+from datasets import load_dataset
+dataset = load_dataset('json',data_files=dataSrc, split="train")  # 从 Hugging Face Hub 加载数据集
+
+alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+        ### Input:
+        {}
+
+        ### Response:
+        {}"""
+def formatting_prompts_func(examples):
+    # instructions = examples["instruction"]
+    inputs = examples["input"]
+    outputs = examples["output"]
+    texts = []
+    for input, output in zip(inputs, outputs):
+        text = alpaca_prompt.format(input, output)+my_trainer.tokenizer.eos_token    # 必须添加 EOS_TOKEN 这个特殊符号，否则生成会无限循环。。
+        texts.append(text)
+    return {"text": texts, }
+train_dataset,eval_dataset = my_trainer.load_data(dataset,alpaca_prompt,formatting_prompts_func)
+my_trainer.createTrainner(train_dataset,eval_dataset)
+my_trainer.startTrainAndSave(saveModel)
+
--- src/train.py
+++ src/train.py
@@ -8,19 +8,17 @@ from transformers import TrainingArguments
 import wandb
 
 torch.cuda.empty_cache()
-
 class Tranner:
-    def __init__(self,model_name,datasetSrc,max_seq_length=1024,load_in_4bit=True,TUNE_MODEL_NAME='myModel',TUNE_MODEL_VERSION='v1.0'):
+    def __init__(self,model_name,max_seq_length=1024,load_in_4bit=True,TUNE_MODEL_NAME='myModel',TUNE_MODEL_VERSION='v1.0'):
         self.model_name=model_name
         self.max_seq_length=max_seq_length
         self.dtype=None
         self.load_in_4bit = load_in_4bit # 以4bit加载模型
         self.model=None
         self.tokenizer=None
-        self.TUNE_MODEL_NAME=TUNE_MODEL_NAME
-        self.TUNE_MODEL_VERSION=TUNE_MODEL_VERSION
         self.trainer=None
-        self.datasetSrc=datasetSrc
+        self.TUNE_MODEL_NAME =TUNE_MODEL_NAME
+        self.TUNE_MODEL_VERSION=TUNE_MODEL_VERSION
     def loadModel(self):
         print('loading',self.model_name)
         model, tokenizer = FastLanguageModel.from_pretrained(
@@ -33,6 +31,7 @@ class Tranner:
         self.model=model
         self.tokenizer=tokenizer
         print('loading finish',self.model_name)
+        self.get_peft_model(self.model)
     def get_peft_model(self,model):
         self.model = FastLanguageModel.get_peft_model(
             model,
@@ -55,44 +54,14 @@ class Tranner:
             loftq_config=None,  # LoftQ
             # token = "hf_...", # use one if using gated models like meta-llama/Llama-3-8b
         )
-
-    def load_data(self,src):
-        # Alpaca prompt
-        alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
-
-        ### Instruction:
-        {}
-
-        ### Input:
-        {}
-
-        ### Response:
-        {}"""
-
-        def formatting_prompts_func(examples):
-            instructions = examples["instruction"]
-            inputs = examples["input"]
-            outputs = examples["output"]
-            texts = []
-            for instruction, input, output in zip(instructions, inputs, outputs):
-                text = alpaca_prompt.format(instruction, input,
-                                            output) + tokenizer.eos_token  # 必须添加 EOS_TOKEN 这个特殊符号，否则生成会无限循环。。
-                texts.append(text)
-            return {"text": texts, }
-
-        # 加载数据集
-        dataset = load_dataset(src, split="train")  # 从 Hugging Face Hub 加载数据集
-        # random_indices = np.random.choice(len(dataset), 8000, replace=False)  # 随机选择500个样本
-        # dataset = dataset.select(random_indices)
+    def load_data(self,dataset,alpaca_prompt,formatting_prompts_func):
+        self.alpaca_prompt=alpaca_prompt
         dataset = dataset.map(formatting_prompts_func, batched=True, )
         split_index = int(len(dataset) * 0.9)
         train_dataset = dataset.select(range(split_index))  # 前90%用于训练
         eval_dataset = dataset.select(range(split_index, len(dataset)))  # 后10%用于评估
-
         return train_dataset,eval_dataset
-
-    def createTrainner(self):
-        train_dataset, eval_dataset = load_dataset(self.datasetSrc)
+    def createTrainner(self,train_dataset,eval_dataset):
         self.trainer = SFTTrainer(
             model=self.model,
             tokenizer=self.tokenizer,
@@ -107,7 +76,7 @@ class Tranner:
                 per_device_eval_batch_size=1,
                 gradient_accumulation_steps=4,  # 设置为4以避免GPTQ量化问题
                 warmup_steps=5,
-                max_steps=500,  # 微调整体循环次数
+                max_steps=1000,  # 微调整体循环次数
                 eval_steps=50,  # 每多少次循环评估一次
                 learning_rate=2e-4,  # 建议 2e-4 用于4位int量化，1e-4 用于8位int量化
                 fp16=not torch.cuda.is_bf16_supported(),
@@ -126,7 +95,7 @@ class Tranner:
             ),
         )
 
-    def startTrainAndSave(self,save_method='merged_4bit_forced'):
+    def startTrainAndSave(self,dir_path='myModel-v1.0',save_method='merged_4bit_forced'):
         trainer_stats = self.trainer.train()
-        self.model.save_pretrained_merged(self.TUNE_MODEL_NAME + '-' + self.TUNE_MODEL_VERSION, self.tokenizer,save_method)
+        self.model.save_pretrained_merged(dir_path, self.tokenizer,save_method)
         wandb.finish()
